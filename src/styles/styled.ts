9c0608abb6608814a092f99d196440098d1c1938
sara hakimi <sarahakimi2000@yahoo.com>
2025-08-20 00:19:02 +0200

    feat: add landing page

--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -48,6 +48,6 @@ export const Button = styled.button`
 `;
 
 declare module 'styled-components' {
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
